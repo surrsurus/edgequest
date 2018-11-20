e1a90f6f4c0c1d859d583d582ac89d968a215d94
surrsurus <graymury@gmail.com>
2018-11-20 10:24:14 -0500

    Add rotation to structs

--- src/core/world/dungeon/builder/structure.rs
+++ src/core/world/dungeon/builder/structure.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 use self::rand::Rng;
 
+// Read files
 use std::io::prelude::*;
 use std::fs;
 
@@ -11,10 +12,9 @@ use core::world::dungeon::map::{self, tile, Tile};
 use core::renderer::RGB;
 
 ///
-/// Simple dungeon builder
-///
-/// This builder places a number of small rooms (respective to map size)
-/// all connected by corridors.
+/// Structure placer
+/// 
+/// Generate prefab structures based on files and place them on the grid
 ///
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Structure {
@@ -29,8 +29,6 @@ impl Structure {
   /// Add a random structure
   ///
   fn add_rand_struct(&mut self) {
-
-    // Okay. Hang on
 
     // RNG
     let mut rng = rand::thread_rng();
@@ -47,12 +45,15 @@ impl Structure {
     // Read to string
     file.read_to_string(&mut s).unwrap();
 
-    // Turn string to 2d array of tiles
+    // Prepare method to store data read from file
     let mut strct : map::Grid<Tile> = vec![];
     let mut line : Vec<Tile> = vec![];
 
+    // Read file as characters
     for c in s.chars() {
+      // If not a newline
       if c != '\n' {
+        // Match tile based on character
         let tile = {
           match c {
             '#' => Tile::new("Wall", ' ', RGB(40, 40, 40), RGB(33, 33, 33), tile::Type::Wall(tile::Wall::Normal)),
@@ -61,7 +62,9 @@ impl Structure {
             _ => panic!("Unknown character: {}", c)
           }
         };
+        // Push character to the line
         line.push(tile);
+      // If we hit a new line we need to push the line to the tile struct, and empty the line
       } else {
         strct.push(line);
         line = vec![];
@@ -70,6 +73,46 @@ impl Structure {
 
     // Rotate randomly
     // NOTE: Not done
+
+    // (x, y) rotated 90 degrees around (0, 0) is (-y, x).
+    // However, vectors are sized in a way that won't allow for negative indexing.
+    // Our formula for point tranformation should be:
+    // (-y + total x length, x)
+
+    let rot90 = | grid: map::Grid<Tile> | -> map::Grid<Tile> {
+
+      // We could clone but I feel like this way is faster
+      let mut rot_grid = map::Grid::<Tile>::new();
+
+      // Measure x on y axis
+      for x in 0..grid[0].len() {
+
+        // Fill new vecs with init
+        let mut vec = Vec::<Tile>::new();
+
+        // Measure y on x axis
+        for y in 0..grid.len() {
+          vec.push(
+            
+            // Rotation performed by following above function
+            grid[grid.len() - 1 - y][x].clone()
+
+          );
+        }
+
+        rot_grid.push(vec);
+
+      }
+
+      return rot_grid;
+
+    };
+
+    // Perform 1 - 4 rotations
+    // NOTE: 4 rotations = starting positon. Might be a good idea to improve this
+    for _ in 0..rng.gen_range(0, 4) {
+      strct = rot90(strct);
+    }
 
     // Read details of vector
     let w = strct.len();
